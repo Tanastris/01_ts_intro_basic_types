a764ac4020014c6e8606e3e28151255809e55c07
RokasV <vezikauskas.r@gmail.com>
2024-02-07 09:11:36 +0200

    basic types

--- first.js
+++ first.js
@@ -1,2 +1,11 @@
+"use strict";
+// basic types
+// number, string, boolean, null, undefined, void
+Object.defineProperty(exports, "__esModule", { value: true });
 var num = 5;
+num = 10;
+num = 'asd';
+var str = 'James';
+str = true;
+var isAdmin = true;
 console.log('hello ts', num);
