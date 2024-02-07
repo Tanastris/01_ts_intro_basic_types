9446c653f4227bf9a47dff288d9919df4a5cff48
RokasV <vezikauskas.r@gmail.com>
2024-02-07 09:36:45 +0200

    index

---
+++ public/first.js
@@ -0,0 +1,13 @@
+// basic types
+// number, string, boolean, null, undefined, void
+let num = 5;
+num = 10;
+// num = 'asd';
+let str = 'James';
+// str = true;
+let isAdmin;
+isAdmin = false;
+isAdmin = !!0;
+isAdmin = Boolean('0');
+console.log('hello ts', num, isAdmin);
+export {};
---
+++ src/first.ts
@@ -0,0 +1,25 @@
+// basic types
+
+// number, string, boolean, null, undefined, void
+
+let num = 5;
+
+num = 10;
+
+// num = 'asd';
+
+let str = 'James';
+
+// str = true;
+
+let isAdmin: boolean;
+
+isAdmin = false;
+
+isAdmin = !!0;
+
+isAdmin = Boolean('0');
+
+console.log('hello ts', num, isAdmin);
+
+export {};
